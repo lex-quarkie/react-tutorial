eb4a8f27a67634a7ee3462436fd4c59390cef9fc
quarkie <lex.quarkie@gmail.com>
2016-12-29 15:33:36 +0300

    News component added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 console.log(React);
 console.log(ReactDOM);
 
+var News = React.createClass({
+	render: function() {
+		return (
+			<div className="news">
+			К сожалению, новостей нет.
+			</div>
+		);
+	}
+});
+
 var App = React.createClass({
 	render: function() {
 		return (
 			<div className="app">
-			Всем привет, я компонент App!
+			Всем привет, я компонент App! Я умею отображать новости
+			<News />
 			</div>
 			);
 
