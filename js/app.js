2cc6712fe8f0ef36beddac7f3fecf282eeab1e3f
Lex Quarkie <lex.quarkie@gmail.com>
2016-12-30 16:33:45 +0300

    Advanced using ended

--- js/app.js
+++ js/app.js
@@ -1,33 +1,60 @@
-console.log(React);
-console.log(ReactDOM);
-
 var my_news= [
 	{
 		author : 'Keith Flint',
-		text : 'Smack my bitch up'
+		text : 'Smack my bitch up',
+		bigText : 'Smack my bitch up, Change my pitch up' 
 	},
 	{
 		author : 'Kurt Cobain',
-		text : 'I know a dirty word'
+		text : 'Load up the guns',
+		bigText : 'Load up the guns, bring new friends - it\'s fun to lose and to pretend'
 	}
 ];
 
 
 var Article = React.createClass({
+	propTypes: {
+		data: React.PropTypes.shape({
+			author: React.PropTypes.string.isRequired,
+			text: React.PropTypes.string.isRequired,
+			bigText: React.PropTypes.string.isRequired,
+		})
+	},
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+	readmoreClick: function(e) {
+		e.preventDefault();
+		this.setState({visible: true});
+	},
 	render: function() {
 		var author = this.props.data.author,
-				text = this.props.data.text;
+				text = this.props.data.text,
+				bigText = this.props.data.bigText,
+				visible = this.state.visible;
 
 		return (
-			<div className="article">
-				<p className="news__author">{author}:</p>
-				<p className="news__text">{text}</p>
+			<div className='article'>
+				<p className='news__author'>{author}:</p>
+				<p className='news__text'>{text}</p>
+				<a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none': '')}>Подробнее</a>
+				<p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
 			</div>
 		)
 	}
 });
 
 var News = React.createClass({
+	propTypes: {
+		data: React.PropTypes.array.isRequired
+	},
+	getInitialState: function() {
+		return {
+			counter: 0
+		}
+	},
 	render: function() {
 		var data = this.props.data;
 		var newsTemplate;
@@ -47,7 +74,9 @@ var News = React.createClass({
 	return (
 		<div className="news">
 			{newsTemplate}
-			<strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+			<strong 
+				className={'news__count ' + (data.length > 0 ? '':'none') }
+				Всего новостей: {data.length}</strong>
 		</div>
 
 
@@ -61,7 +90,7 @@ var App = React.createClass({
 		return (
 			<div className="app">
 				<h3>Новости</h3>
-				<News data={my_news} />
+				<News data={my_news}/>
 			</div>
 			);
 
